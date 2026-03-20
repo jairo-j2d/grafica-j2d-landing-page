1e273b81be3eb3d4ce3b14d5994e45697190f631
Skip <skip@usecurling.com>
2026-03-20 09:38:35 +0000

    Fix layout error with MainStoreProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,25 @@ import Dashboard from './pages/Dashboard'
 import Layout from './components/Layout'
 import NotFound from './pages/NotFound'
 import { Toaster } from './components/ui/toaster'
+import { MainStoreProvider } from './stores/useMainStore'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route element={<Layout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/pedidos" element={<Dashboard />} />
-          <Route path="/clientes" element={<Dashboard />} />
-          <Route path="/configuracoes" element={<Dashboard />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Toaster />
-    </Router>
+    <MainStoreProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route element={<Layout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/pedidos" element={<Dashboard />} />
+            <Route path="/clientes" element={<Dashboard />} />
+            <Route path="/configuracoes" element={<Dashboard />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Toaster />
+      </Router>
+    </MainStoreProvider>
   )
 }
 export default App
